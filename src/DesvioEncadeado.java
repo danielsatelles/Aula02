e305d74ace15ce47075fdcf756ea69d066f203e5
Daniel Sateles <danielsatelles@gmail.com>
2021-05-19 00:16:52 -0300

    Versão Final da Aula02

--- src/DesvioEncadeado.java
+++ src/DesvioEncadeado.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class DesvioEncadeado {
 
 	public static void main(String[] args) {
-		/*Uma ONG resolveu prestar um serviço bem diferente: ela oferece vans para buscar pessoas com qualquer tipo de dificuldade de locomoção para poderem votar.
-		 * Para evitar problemas no momento do embarque, porém, você foi convidado a criar um programa que valide a idade dos passageiros: caso tenham 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou não. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
+		/*Uma ONG resolveu prestar um serviço bem diferente: ela oferece vans para buscar pessoas com qualquer tipo 
+		 * de dificuldade de locomoção para poderem votar.
+		 * Para evitar problemas no momento do embarque, porém, você foi convidado a criar um programa que valide a 
+		 * idade dos passageiros: caso tenham 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos,
+		 * podem optar por votar ou não. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
 		 * Crie um programa que receba a idade dos passageiros em potencial e indique se podem embarcar e votar.*/
 		
 		Scanner leitor = new Scanner(System.in);
@@ -14,7 +17,16 @@ public class DesvioEncadeado {
 	
 		/*Aqui devemos criar uma solução para realizar as verificações.*/
 		
-		
+		if (idade < 16) {
+			//Aqui o que ocorre se a idade for menor do que 16
+			System.out.println("Você não votar e nem embarcar");
+		} else {
+			if(idade > 18) {
+				System.out.println("Você é obrigado a votar");
+			}else {
+					System.out.println("Você pode optar por votar e embarcar"); 
+			}
+		}
 	
 		leitor.close();
 
--- src/DesvioSimples.java
+++ src/DesvioSimples.java
@@ -14,6 +14,12 @@ public class DesvioSimples {
 		System.out.println("Deixe-me verificar sua idade...");
 		/*Aqui devemos criar uma solução para verificar a idade do usuário*/
 		
+		if(idade >=12) {
+			System.out.println("Você está aprovado para jogar");
+		} else {
+			System.out.println("Infelizmente você não está aprovado para jogar");
+		}
+		
 		leitor.close();
 
 	}
